84d753c1cc09006649defff78d378ec1417bc5ed
Boyapati Dhanush <dhanushboyapati007@gmail.com>
2025-12-10 22:09:35 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,94 @@
+let boxes=document.querySelectorAll(".box");
+let resetbtn=document.querySelector("#reset");
+let msg=document.querySelector(".msg");
+
+
+
+let turnO=true;
+
+const winningPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+
+
+boxes.forEach((box)=>{
+
+    box.addEventListener("click",()=>{
+        console.log("box was clicked");
+        if(turnO){
+        box.innerHTML="O";
+        turnO=false;
+        box.disabled=true;
+
+        }
+        else{
+            box.innerHTML="X";
+            turnO=true;
+         box.disabled=true;
+
+        }
+        winnercheck();
+
+    })
+});
+
+
+const showwinner =(winner)=>{
+    msg.innerText=`con winner is ${winner}`;
+    msg.classList.remove("hide");
+     for(let b of boxes){
+        b.disabled = true;
+    }
+
+}
+
+const winnercheck = () =>{
+    for(let pattern of winningPatterns)
+    {
+
+       let pos1= boxes[pattern[0]].innerText;
+       let pos2 = boxes[pattern[1]].innerText;
+       let pos3 = boxes[pattern[2]].innerText
+   
+
+     if(pos1!=""&& pos2!=""&& pos3!="")
+     {
+        if(pos1===pos2&&pos2===pos3)
+        {
+            console.log("winner",pos1);
+            showwinner(pos1);
+        }
+     }
+
+
+
+        
+        
+
+
+    }
+};
+
+const ensable =() =>{
+    for( let bo of boxes)
+    {
+        bo.disabled=false;
+        bo.innerText="";
+        msg.classList.add("hide");
+    }
+}
+
+const res =()=>{
+    turnO=true;
+    ensable();
+    
+}
+resetbtn.addEventListener("click",res);
